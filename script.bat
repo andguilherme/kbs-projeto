3134f9c426b28f3d3a377a64eced985537633568
andguilherme <andersongui@gmail.com>
2022-12-13 19:49:58 -0300

    update

--- script.bat
+++ script.bat
@@ -7,3 +7,11 @@ echo "realizando push das imagens...."
 
 docker push andersongui/projeto-backend:1.0
 docker push andersongui/projeto-database:1.0
+
+echo "Criando serviços no cluster kubernetes...."
+
+kubectl apply -f  ./services.yml
+
+echo "Criando deployments...."
+
+kubectl apply -f  ./deployment.yml
